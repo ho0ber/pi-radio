79e1ab260087ccd366e78691af54475332f5dd2f
Samuel Colburn <ho0ber@gmail.com>
2025-06-14 12:25:24 -0400

    rpi funtimes

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install git mpg321 python3 python3-pip -y
 git pull
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 source env.sh
 
 while :; do
