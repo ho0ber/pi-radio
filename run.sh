948235c97552db8f462c30b83c9e565d95b28dc9
Samuel Colburn <ho0ber@gmail.com>
2025-06-14 12:19:54 -0400

    no need to venv on an rpi

--- run.sh
+++ run.sh
@@ -2,10 +2,9 @@
 
 sudo apt-get install git mpg321 python3 -y
 git pull
-source venv/bin/activate
-pip install -r requirements.txt
-
+python3 -m install -r requirements.txt
 source env.sh
+
 while :; do
     python radio.py
     sleep 1
